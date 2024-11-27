4b18444de287c115ff4fe5b9fcc62e02296ad27f
Alain Nicolas <alain.nicolas@consensys.net>
2024-11-27 16:06:16 +0100

    chore: Update Verax SDK + Infura API key

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,7 +1,9 @@
-import { arbitrum, arbitrumSepolia, base, baseSepolia, linea, lineaSepolia, mainnet } from 'wagmi/chains';
+import { arbitrumSepolia, baseSepolia, bscTestnet, lineaSepolia, mainnet } from 'wagmi/chains';
 import { defaultWagmiConfig } from '@web3modal/wagmi';
+import { http } from 'wagmi';
 
 export const walletConnectProjectId = '1640042ae61e8c357b5b6034df2b7821';
+const infuraApiKey: string = '2VbuXFYphoB468fyFPinOmis7o5';
 
 const metadata = {
   name: 'Verax | Tutorial',
@@ -10,9 +12,16 @@ const metadata = {
   icons: ['https://tutorial.examples.ver.ax/favicon.ico'],
 };
 
-const chains = [lineaSepolia, linea, arbitrumSepolia, arbitrum, baseSepolia, base, mainnet] as const;
+const chains = [lineaSepolia, arbitrumSepolia, baseSepolia, bscTestnet, mainnet] as const;
 export const wagmiConfig = defaultWagmiConfig({
-  chains,
   projectId: walletConnectProjectId,
   metadata,
+  chains,
+  transports: {
+    [lineaSepolia.id]: http(`https://linea-sepolia.infura.io/v3/${infuraApiKey}`),
+    [arbitrumSepolia.id]: http(`https://arbitrum-sepolia.infura.io/v3/${infuraApiKey}`),
+    [baseSepolia.id]: http(`https://base-sepolia.infura.io/v3/${infuraApiKey}`),
+    [bscTestnet.id]: http(`https://bsc-testnet.infura.io/v3/${infuraApiKey}`),
+    [mainnet.id]: http(`https://mainnet.infura.io/v3/${infuraApiKey}`),
+  },
 });
